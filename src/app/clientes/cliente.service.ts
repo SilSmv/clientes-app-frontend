2a320b9e5fd17e33043c21338c8d6b8efa1db051
SilSmv <silvana.munoz.vargas@gmail.com>
2022-09-21 14:48:27 -0400

    Create client

--- src/app/clientes/cliente.service.ts
+++ src/app/clientes/cliente.service.ts
@@ -2,16 +2,26 @@ import { Injectable } from '@angular/core';
 import { Cliente } from './cliente';
 //import {CLIENTES} from './clientes.json';
 import {Observable} from 'rxjs';
-import {HttpClient} from '@angular/common/http';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
 //import {map} from 'rxjs/operators';
 
 @Injectable()
 export class ClienteService {
   private urlEndPoint:string = 'http://localhost:8080/api/clientes';
 
+  private httpHeaders = new HttpHeaders({'Content-Type': 'application/json'});
+
   constructor(private http: HttpClient) { }
   getClientes(): Observable <Cliente[]>{
     //return of(CLIENTES);
-    return this.http.get<Cliente[]>(this.urlEndPoint)
+    return this.http.get<Cliente[]>(this.urlEndPoint);
   }
+
+  create(cliente:Cliente) : Observable<Cliente>{
+    return this.http.post<Cliente>(this.urlEndPoint, cliente,{headers: this.httpHeaders})
+  }
+  getCliente(id: any): Observable<Cliente>{
+    return this.http.get<Cliente>(`${this.urlEndPoint}/${id}`)
+  }
+
 }
--- src/app/clientes/form.component.ts
+++ src/app/clientes/form.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import {Cliente} from './cliente'
+import {ClienteService} from './cliente.service'
+import {Router, ActivatedRoute} from '@angular/router'
+import swal from 'sweetalert2'
+
 
 @Component({
   selector: 'app-form',
@@ -10,14 +14,27 @@ export class FormComponent implements OnInit {
   public cliente: Cliente = new Cliente()
   public titulo: string = "Crear Cliente"
 
-  constructor() { }
+  constructor(private ClienteService: ClienteService,
+  private router: Router,
+  private activatedRoute: ActivatedRoute) { }
 
   ngOnInit(): void {
   }
 
+  cargarCliente():void{
+
+  }
+
   public create(): void{
+    this.ClienteService.create(this.cliente).subscribe(
+      cliente => {
+        this.router.navigate(['/clientes'])
+        swal.fire('Nuevo Cliente',`Cliente ${cliente.nombre} creado con exito`, 'success')
+      }
+    );
     console.log("Cliked")
     console.log(this.cliente)
   }
+
 
 }
